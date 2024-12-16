c38d11be8baf05c70d38b8d251033136027dd278
KotiYadav <koti0120@gmail.com>
2024-12-16 13:24:47 +0530

    arguments

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1 #no space between = and value
+PERSON2=$2 #no space between = and value
+echo "$PERSON1:: Hi $PERSON2 how are you"
+echo "$PERSON2:: i'm fine $PERSON1 what about you"
+echo "$PERSON1:: i'm fine too . how is your work..?"
+echo "$PERSON2:: not bad"
