5c751d3c19d4c05542964018250ea1cc488a0b25
KotiYadav <koti0120@gmail.com>
2024-12-16 13:05:31 +0530

    declaring variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "venkat:: Hi koti how are you"
-echo "koti:: i'm fine venkat what about you"
-echo "venkat:: i'm fine too . how is your work..?"
-echo "koti:: not bad"
+PERSON1=venkat #no space between = and value
+PERSON2=koti #no space between = and value
+echo "$PERSON1:: Hi $PERSON2 how are you"
+echo "$PERSON2:: i'm fine $PERSON1 what about you"
+echo "$PERSON1:: i'm fine too . how is your work..?"
+echo "$PERSON2:: not bad"
