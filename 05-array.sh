083e04e2c9cf49f695de65e73299b503a24083e3
KotiYadav <koti0120@gmail.com>
2024-12-16 14:51:24 +0530

    Array

--- 05-array.sh
+++ 05-array.sh
@@ -2,6 +2,8 @@
 
 MOVIES=("puspa-2" "RRR" "bahubali" "kalki")
 #list always starts with 0
+#size of above array is 4
+#index are 0,1,2,3
 echo "first movie is: ${MOVIES[0]}"
 echo "second movie is: ${MOVIES[1]}"
 echo "all movies is: ${MOVIES[@]}"
