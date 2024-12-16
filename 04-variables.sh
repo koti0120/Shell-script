6aecef36a3958070c77c225dda11cb57eb5eef62
KotiYadav <koti0120@gmail.com>
2024-12-16 14:24:28 +0530

    confidentials

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "enter username::"
+read -s USERNAME # s is used for confindentials hiding purpose
+
+echo "enter password:::"
+read -s PASSWORD
+
+echo "username is $USERNAME and password is $PASSWORD"
