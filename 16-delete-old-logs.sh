b5460016488fc17439b24092f7c3bde91e18c660
KotiYadav <koti0120@gmail.com>
2024-12-18 22:52:13 +0530

    set command

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SOURCE_DIRECTORY=/tmp/app-logs
-R="e\[31m"
-G="e\[32m"
-Y="e\[33m"
-N="e\[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ -d $SOURCE_DIRECTORY ] #here -d means this test for directory
 then
