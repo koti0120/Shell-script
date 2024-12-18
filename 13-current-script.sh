135bceb1e0c09122e4c9c8e9b069d72036e10488
KotiYadav <koti0120@gmail.com>
2024-12-18 15:47:23 +0530

    call other script

--- 13-current-script.sh
+++ 13-current-script.sh
@@ -4,6 +4,8 @@ COURSE="Devops from current script"
 echo "before calling other script course: $Course"
 echo "process id of current shell script: $$"
 
-./14-other-script.share
+# ./14-other-script.sh
+source ./14-other-script.sh
+
 
 echo "after calling other script course: $Course"
